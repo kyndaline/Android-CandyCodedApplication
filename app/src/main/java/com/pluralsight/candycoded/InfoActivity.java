975b40d6f2dba949ae9a3c1e1db736cc1a230c7c
kyndaline <52834132+kyndaline@users.noreply.github.com>
2019-10-30 09:45:46 +0000

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -22,20 +22,21 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
+    public void createMapIntent(View view){
+        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent map_intent = new Intent(Intent.ACTION_VIEW, uriAddress);
+        map_intent.setPackage("com.google.android.apps.maps");
+
+        if( map_intent.resolveActivity(getPackageManager()) != null){
+            startActivity(map_intent);
+        }
+
+    }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
-      public void createMapIntent(View view){
-        Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
-        mapIntent.setPackage("com.google.android.apps.maps");
-
-        if( mapIntent.resolveActivity(getPackageManager()) != null){
-            startActivity(mapIntent);
-        }
-
-    }
+      
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
